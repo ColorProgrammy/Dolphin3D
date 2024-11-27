b543c1ac5410bd41a754bc3493b172bdd7ca19d5
yupi <aconmonuser@gmail.com>
2024-11-27 21:35:23 +0300

    Vector2

--- include/Vectors/Vector2.h
+++ include/Vectors/Vector2.h
@@ -1,3 +1,13 @@
+/*
+File: Vector2.h
+Developer: ColorProgrammy
+
+Description:
+Creating Vector2
+*/
+
+// Please do not touch this code.
+
 #pragma once
 
 struct vec2
@@ -7,8 +17,19 @@ struct vec2
     vec2(float value) : x(value), y(value) {}
     vec2(float _x, float _y) : x(_x), y(_y) {}
 
-    vec2 operator+(vec2 const& other) { return vec2(x + other.x, y + other.y); }
-    vec2 operator-(vec2 const& other) { return vec2(x - other.x, y - other.y); }
-    vec2 operator*(vec2 const& other) { return vec2(x * other.x, y * other.y); }
-    vec2 operator/(vec2 const& other) { return vec2(x / other.x, y / other.y); }
+    vec2 operator+(vec2 const& second) { 
+        return vec2(x + second.x, y + second.y); 
+    }
+
+    vec2 operator-(vec2 const& second) { 
+        return vec2(x - second.x, y - second.y); 
+    }
+
+    vec2 operator*(vec2 const& second) {
+        return vec2(x * second.x, y * second.y);
+    }
+
+    vec2 operator/(vec2 const& second) { 
+        return vec2(x / second.x, y / second.y); 
+    }
 };
