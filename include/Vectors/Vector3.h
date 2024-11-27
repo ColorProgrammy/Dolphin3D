4cb689d33425bb8f4402f720debf2dec96d7db60
yupi <aconmonuser@gmail.com>
2024-11-27 21:40:05 +0300

    Vector3

--- include/Vectors/Vector3.h
+++ include/Vectors/Vector3.h
@@ -1,3 +1,13 @@
+/*
+File: Vector3.h
+Developer: ColorProgrammy
+
+Description:
+Creating Vector3
+*/
+
+// Please do not touch this code.
+
 #include "Vector2.h"
 
 struct vec3
@@ -8,10 +18,23 @@ struct vec3
     vec3(float _x, vec2 const& v) : x(_x), y(v.x), z(v.y) {};
     vec3(float _x, float _y, float _z) : x(_x), y(_y), z(_z) {};
 
-    vec3 operator+(vec3 const& other) { return vec3(x + other.x, y + other.y, z + other.z); }
-    vec3 operator-(vec3 const& other) { return vec3(x - other.x, y - other.y, z - other.z); }
-    vec3 operator*(vec3 const& other) { return vec3(x * other.x, y * other.y, z * other.z); }
-    vec3 operator/(vec3 const& other) { return vec3(x / other.x, y / other.y, z / other.z); }
-    vec3 operator-() { return vec3(-x, -y, -z); }
+    vec3 operator+(vec3 const& second) { 
+        return vec3(x + second.x, y + second.y, z + second.z); 
+    }
 
+    vec3 operator-(vec3 const& second) { 
+        return vec3(x - second.x, y - second.y, z - second.z); 
+    }
+
+    vec3 operator*(vec3 const& second) { 
+        return vec3(x * second.x, y * second.y, z * second.z); 
+    }
+
+    vec3 operator/(vec3 const& second) { 
+        return vec3(x / second.x, y / second.y, z / second.z); 
+    }
+
+    vec3 operator-() {
+        return vec3(-x, -y, -z); 
+    }
 };
