fafde6b9c7857369360dba196c750d3e3f864ca5
yupi <aconmonuser@gmail.com>
2025-01-21 21:59:46 +0300

    Update render.h

--- include/render.h
+++ include/render.h
@@ -3,11 +3,10 @@
 
 #include <windows.h>
 #include <vector>
-#include "include/color.h"
-#include "include/Vector3.h"
+#include "color.h"
+#include "Vector3.h"
 
 class Object;
-
 
 // Function declaration, use extern to ensure a single definition in source file.
 extern CHAR_INFO* currentBuffer;
@@ -20,5 +19,6 @@ bool initRender(int width, int height);
 void render(int width, int height);
 void renderColor(int i, int j, int width, size_t gradientSize, const char* gradient, bool hit, Color currentcolor, float brightness);
 void renderObjects(std::vector<Object*>& objects, vec3& ro, vec3& rd, bool& hit, Color& currentcolor, float& brightness, vec3& normal, vec3& light, float lightRadius);
+vec2 createUV(int i, int j, int width, int height, float aspect, float pixelAspect);
 
 #endif
