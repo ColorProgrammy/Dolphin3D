db3f4ee33e912e5f5942e7dad90ccfef7a931e66
yupi <aconmonuser@gmail.com>
2025-01-22 21:59:37 +0300

    Update render.h

--- include/render.h
+++ include/render.h
@@ -18,7 +18,7 @@ void freeObjects(std::vector<Object*>& objects);
 bool initRender(int width, int height);
 void render(int width, int height);
 void renderColor(int i, int j, int width, size_t gradientSize, const char* gradient, bool hit, Color currentcolor, float brightness);
-void renderObjects(std::vector<Object*>& objects, vec3& ro, vec3& rd, bool& hit, Color& currentcolor, float& brightness, vec3& normal, vec3& light, float lightRadius);
-vec2 createUV(int i, int j, int width, int height, float aspect, float pixelAspect);
+void renderObjects(std::vector<Object*>& objects, vec3& ro, vec3& rd, bool& hit, Color& currentcolor, float& brightness, vec3& normal, vec3& light);
+vec2 createUV(int i, int j, int width, int height);
 
 #endif
