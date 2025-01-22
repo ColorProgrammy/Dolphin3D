7af3e7c46e0327bf0b049e641f278e9b9aa745ba
yupi <aconmonuser@gmail.com>
2025-01-22 21:58:51 +0300

    Add files via upload

---
+++ include/gradients.h
@@ -0,0 +1,11 @@
+﻿#ifndef GRADIENTS_H
+#define GRADIENTS_H
+
+#define GRADIENT_0 " .,:!/rH$#@"
+
+#define GRADIENT_1 " .:-=+*#%@"
+#define GRADIENT_2 " `´-!/?7ilI#%@"
+#define GRADIENT_3 " .:;i1tfLCG08@"
+// you can add more at your discretion
+
+#endif // GRADIENTS_H
