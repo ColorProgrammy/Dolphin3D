666e8a5b0753c5ca6eedb21740d42ae99e23e8e2
yupi <aconmonuser@gmail.com>
2025-01-23 21:56:21 +0300

    Update gradients.h

--- include/gradients.h
+++ include/gradients.h
@@ -1,11 +1,17 @@
-﻿#ifndef GRADIENTS_H
+#ifndef GRADIENTS_H
 #define GRADIENTS_H
+
+#include <cstddef>
 
 #define GRADIENT_0 " .,:!/rH$#@"
 
 #define GRADIENT_1 " .:-=+*#%@"
-#define GRADIENT_2 " `´-!/?7ilI#%@"
+#define GRADIENT_2 " .:-!/?7ilI#%@"
 #define GRADIENT_3 " .:;i1tfLCG08@"
 // you can add more at your discretion
+
+inline void setGradientSize(char gradient[], size_t& gradientSize) {
+	gradientSize = strlen(gradient) - 1;
+}
 
 #endif // GRADIENTS_H
