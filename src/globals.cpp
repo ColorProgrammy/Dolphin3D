7a3e634825a7e4f875eaa53631e320916f83cab7
yupi <aconmonuser@gmail.com>
2025-01-25 20:19:53 +0300

    Update globals.cpp

--- src/globals.cpp
+++ src/globals.cpp
@@ -1,4 +1,9 @@
-#include "include/globals.h"
+#include "../include/globals.h"
+#include "../include/log.h"
+#include <Shlobj.h>
 
-// Define hConsole here - only one definition is needed.
 HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+std::string configPath = "config.ini";
+std::string folderName = "Example";
+
+std::wstring Log::logPath;
