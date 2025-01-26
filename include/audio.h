7fb14ea43e9f21513b55f4a9e846e4fd45c49b20
yupi <aconmonuser@gmail.com>
2025-01-26 20:42:56 +0300

    Update audio.h

--- include/audio.h
+++ include/audio.h
@@ -6,29 +6,26 @@ Description:
 Library for playing audio
 */
 
+#define _CRT_SECURE_NO_WARNINGS
 #include <windows.h>
 #pragma comment(lib, "winmm.lib")
 
-void playSoundAsync(const char* soundFile) {
-	char currentWorkingDirectory[MAX_PATH];
+inline void playSoundAsync(const char* soundFile) {
+    char currentWorkingDirectory[MAX_PATH];
     GetCurrentDirectory(MAX_PATH, currentWorkingDirectory);
-
     char fullSoundPath[MAX_PATH];
-    strcpy_s(fullSoundPath, currentWorkingDirectory);
-    strcat_s(fullSoundPath, "\\");
-    strcat_s(fullSoundPath, soundFile);
-
+    strcpy_s(fullSoundPath, MAX_PATH, currentWorkingDirectory);
+    strcat_s(fullSoundPath, MAX_PATH, "\\");
+    strcat_s(fullSoundPath, MAX_PATH, soundFile);
     PlaySound(TEXT(fullSoundPath), NULL, SND_ASYNC | SND_FILENAME);
 }
 
-void playSoundSync(const char* soundFile) {
-	char currentWorkingDirectory[MAX_PATH];
+inline void playSoundSync(const char* soundFile) {
+    char currentWorkingDirectory[MAX_PATH];
     GetCurrentDirectory(MAX_PATH, currentWorkingDirectory);
-
     char fullSoundPath[MAX_PATH];
-    strcpy_s(fullSoundPath, currentWorkingDirectory);
-    strcat_s(fullSoundPath, "\\");
-    strcat_s(fullSoundPath, soundFile);
-
+    strcpy_s(fullSoundPath, MAX_PATH, currentWorkingDirectory);
+    strcat_s(fullSoundPath, MAX_PATH, "\\");
+    strcat_s(fullSoundPath, MAX_PATH, soundFile);
     PlaySound(TEXT(fullSoundPath), NULL, SND_SYNC | SND_FILENAME);
 }
