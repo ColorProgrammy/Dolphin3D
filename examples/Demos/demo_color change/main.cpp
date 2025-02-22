e5554181cbbf12d4fab17eba5fb8dbfe764f53e0
yupi <aconmonuser@gmail.com>
2025-02-22 21:13:28 +0300

    Update main.cpp

--- examples/Demos/demo_color change/main.cpp
+++ examples/Demos/demo_color change/main.cpp
@@ -6,7 +6,7 @@
 #include <conio.h>
 #include "../include/Dolphin3D.h"
 
-// Continuous color change function
+
 DWORD WINAPI changeBoxColor(LPVOID lpParam) {
     Box* box = static_cast<Box*>(lpParam);
     while (true) {
@@ -25,55 +25,76 @@ DWORD WINAPI changeBoxColor(LPVOID lpParam) {
 }
 
 int main() {
+    Config cfg;
+    if (!setConfig(folderName, configPath, cfg)) {
+        return 1;
+    }
+
     bool hit;
     Color currentcolor;
     vec3 normal;
 
-    Config cfg = readConfig("config.ini");
-
     int width = cfg.width;
     int height = cfg.height;
 
-    setBuffer(width, height);
 
+    setBuffer(width, height);
     initRender(width, height);
     setWindow(width, height, cfg.title);
 
+	setIcon(cfg, folderName);
+
     char gradient[] = GRADIENT_0;
     size_t gradientSize = 0;
+
 	setGradientSize(gradient, gradientSize);
-	
+
     vec3 boxPos = vec3(0, 0, -2);
     vec3 planePos = vec3(0, 0, -3);
+    vec3 spherePos = vec3(-4, 0, -1);
+    vec3 capPos = vec3(0, 0, -7);
     vec3 light = vec3(0, 20, -7.0);
 
     std::vector<Object*> objects;
     objects.push_back(new Plane(planePos, vec3(0, 1, 0), 0.5f, Color::White()));
+    objects.push_back(new Sphere(spherePos, 1.5f, 2.0f, Color::Yellow()));
     
-	Box* testBox = new Box(boxPos, vec3(2,2,2), 1.0f, Color::White());
+	Box* testBox = new Box(boxPos, vec3(2,2,2), 1.0f, Color::Blue());
     objects.push_back(testBox);
+
+	vec3 cylinderA(6.0f, 0.0f, -1.0f);
+    vec3 cylinderB(6.0f, 2.0f, -1.0f);
+    float cylinderRadius = 1.5f;
+    objects.push_back(new Cylinder(cylinderA, cylinderB, cylinderRadius, 2.0f, Color::Green()));
 
 	HANDLE colorChangeThread = CreateThread(NULL, 0, changeBoxColor, testBox, 0, NULL);
 
+	
     for (int t = 1; t > 0; ++t) {
-        swapBuffers(currentBuffer, displayBuffer);
+        swapBuffers(currentBuffer, displayBuffer, width, height);
 
         for (int j = 0; j < height; ++j) {
             for (int i = 0; i < width; ++i) {
+
                 float brightness = 1.0f;
 
-                vec2 uv = createUV(i, j, width, height, aspect, pixelAspect);
+                vec2 uv = createUV(i, j, width, height);
 
                 vec3 rd = norm(vec3(1, uv));
-                vec3 ro = vec3(-7, 0, -1);
+                vec3 ro = vec3(-7, 0, -1);		
+
+                rd = rotateAroundCenterY(rd, 0.5f);
+                ro = rotateAroundCenterY(ro, 0.5f);
+                ro = rotateAroundCenterZ(ro, t * 0.01f);
+                rd = rotateAroundCenterZ(rd, t * 0.01f);
 
                 renderObjects(objects, ro, rd, hit, currentcolor, brightness, normal, light);
                 renderColor(i, j, width, gradientSize, gradient, hit, currentcolor, brightness);
             }
         }
-        render(width, height);
+        render(width, height, 60);
     }
-
+	CloseHandle(colorChangeThread);
 	WaitForSingleObject(colorChangeThread, INFINITE);
     freeBuffers();
     freeObjects(objects);
