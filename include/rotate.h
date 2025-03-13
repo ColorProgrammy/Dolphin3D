f82222f048fb6b92e3c4192040e8b3b4f175d2ee
yupi <aconmonuser@gmail.com>
2025-03-13 21:33:03 +0300

    Update rotate.h

--- include/rotate.h
+++ include/rotate.h
@@ -2,9 +2,6 @@
 
 #include <cmath>
 #include "functions.h"
-#include "mat4.h"
-
-mat4 transform;
 
 inline vec3 rotateAroundCenterX(vec3 a, double angle)
 {
@@ -52,35 +49,4 @@ inline vec3 rotateAroundZ(vec3 a, double angle, vec3 origin)
     b.x = b.x * cos(float(angle)) - b.y * sin(float(angle));
     b.y = b.x * sin(float(angle)) + b.y * cos(float(angle));
     return b + origin;
-}
-
-inline mat4 getRotationMatrix() {
-    mat4 rotation = transform;
-    
-    rotation.m[0][3] = 0.0f;
-    rotation.m[1][3] = 0.0f;
-    rotation.m[2][3] = 0.0f;
-    rotation.m[3][3] = 1.0f;
-    return rotation;
-}
-
-
-inline void rotateLocalX(vec3 pos, float angle) {
-	mat4 rotation = getRotationMatrix() * mat4::rotateX(angle);
-	transform = mat4::translate(pos) * rotation;
-}
-
-inline void rotateLocalY(vec3 pos, float angle) {
-	mat4 rotation = getRotationMatrix() * mat4::rotateY(angle);
-	transform = mat4::translate(pos) * rotation;
-}
-
-inline void rotateLocalZ(vec3 pos, float angle) {
-	mat4 rotation = getRotationMatrix() * mat4::rotateZ(angle);
-	transform = mat4::translate(pos) * rotation;
-}
-
-inline void translate(mat4 transform, const vec3& t, vec3 pos) {
-    transform = mat4::translate(t) * transform;
-    pos += t;
 }
