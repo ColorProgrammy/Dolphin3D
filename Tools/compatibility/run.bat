2f2fdec559b31b255610910195474ab5aab9c981
yupi <aconmonuser@gmail.com>
2026-01-25 19:13:18 +0300

    Update run.bat

--- Tools/compatibility/run.bat
+++ Tools/compatibility/run.bat
@@ -2,10 +2,11 @@
 title Dolphin3D Compatibility Checker
 cls
 
-echo Dolphin3D Compatibility Checker v4.0
+echo Dolphin3D Compatibility Checker v1.0
 echo =====================================
 echo.
 echo Running...
 echo.
 
 cscript //nologo "check.vbs" %*
+
